b25430566c5386e2aa843fd67f8f7c7feb0483a1
Pedro Soares <pedrinhocef@gmail.com>
2017-12-04 21:16:08 -0200

    Add soma de receitas

--- app/src/main/java/com/example/pedri/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/pedri/financask/ui/activity/ListaTransacoesActivity.kt
@@ -3,10 +3,12 @@ package com.example.pedri.financask.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.pedri.financask.R
+import com.example.pedri.financask.extension.formataParaBrasileiro
 import com.example.pedri.financask.model.Tipo
 import com.example.pedri.financask.model.Transacao
 import com.example.pedri.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.resumo_card.*
 import java.math.BigDecimal
 
 
@@ -19,8 +21,21 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
         val transacoes = transacoesDeExemplo()
 
+        adicionaReceitaNoResumo(transacoes)
+
         configuraTransacao(transacoes)
 
+    }
+
+    private fun adicionaReceitaNoResumo(transacoes: List<Transacao>) {
+        var totalReceita = BigDecimal.ZERO
+        for (transacao in transacoes) {
+            if (transacao.tipo == Tipo.RECEITA) {
+                totalReceita = totalReceita.plus(transacao.valor)
+            }
+
+            resumo_card_receita.text = totalReceita.formataParaBrasileiro()
+        }
     }
 
     private fun configuraTransacao(transacoes: List<Transacao>) {
@@ -29,7 +44,7 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
     private fun transacoesDeExemplo(): List<Transacao> {
         return listOf(Transacao(valor = BigDecimal(20.5),
-                tipo = Tipo.DESPESA),
+                tipo = Tipo.RECEITA),
                 Transacao(valor = BigDecimal(900),
                         categoria = "Salario de qualque mes mesmo",
                         tipo = Tipo.RECEITA))
